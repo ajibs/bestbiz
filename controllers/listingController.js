5f1f6382d92424557eac3301157d83b939b3fdf9
bolu ajibs <bolu.ajibawo@gmail.com>
2017-11-24 18:18:10 +0100

    user can view listing and search via name

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,11 +1,24 @@
 const Business = require('../models/Business');
 
 
+exports.showHome = async (req, res) => {
+  const listings = await Business.find({})
+    .sort({ _id: -1 }) // sort according to the most recent
+    .limit(6);
+
+  res.render('index', {
+    title: 'Home',
+    listings
+  });
+};
+
+
 exports.showListingForm = (req, res) => {
   res.render('create-listing', {
     title: 'Create Listing'
   });
 };
+
 
 function extractCategories(data, result) {
   const arrayOptions = data.options.split(',');
@@ -16,6 +29,7 @@ function extractCategories(data, result) {
   return result;
 }
 
+
 exports.addNewListing = async (req, res) => {
   const listing = new Business(req.body);
   const formattedListing = extractCategories(req.body, listing);
@@ -24,4 +38,80 @@ exports.addNewListing = async (req, res) => {
 
   req.flash('success', 'Listing Created Successfully');
   res.redirect('/profile');
+};
+
+
+exports.showSingleListing = async (req, res) => {
+  const listing = await Business.findOne({ _id: req.params.id });
+
+  if (!listing) {
+    req.flash('failed', 'Error! listing not found');
+    res.redirect('back');
+    return;
+  }
+
+  res.render('listing-details', {
+    title: 'Listing Details',
+    listing
+  });
+};
+
+
+exports.showExplore = async (req, res) => {
+  const listings = await Business.find({})
+    .sort({ _id: -1 })
+    .limit(18);
+
+  res.render('explore', {
+    title: 'Explore',
+    listings
+  });
+};
+
+
+exports.searchListings = async (req, res) => {
+  let { name } = req.body;
+  name = name.toLowerCase();
+  const listings = await Business.find({ name });
+
+  if (!listings.length) {
+    req.flash('failed', 'Listings not found for that query');
+    res.redirect('back');
+    return;
+  }
+
+  res.render('explore', {
+    title: 'Explore',
+    listings
+  });
+};
+
+
+exports.seedDB = async (req, res) => {
+  // create demo data
+  const demo = {
+    name: 'devcenter',
+    address: 'lagos, nigeria',
+    description: 'hire a verified african software developer',
+    website: 'http://devcenter.co',
+    email: 'hello@devcenter.co',
+    phone: '+23412345678',
+    categories: [
+      'software development',
+      'design',
+      'user Business'
+    ]
+  };
+
+  Business.remove({}, () => { // empty database then save documents
+    let i = 0;
+    while (i < 25) {
+      const listing = new Business(demo);
+      listing.save();
+      i++;
+    }
+  });
+
+  req.flash('success', 'Database Seeded Successfully');
+  res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const listingController = require('../controllers/listingController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 
-router.get('/', (req, res) => {
-  res.send('hello world');
-});
+router.get('/', catchErrors(listingController.showHome));
 
 /*
 router.get('/signup', userController.showSignup);
@@ -18,7 +16,6 @@ router.post(
   authController.signup
 );
 */
-
 router.get('/login', userController.showLogin);
 router.post('/login', authController.login);
 
@@ -35,12 +32,20 @@ router.get(
 );
 router.post(
   '/create-listing',
+  authController.isLoggedIn,
   userController.validateListing,
   userController.sanitizeData,
   catchErrors(listingController.addNewListing)
 );
 
+
+router.get('/listing/:id', catchErrors(listingController.showSingleListing));
+
+router.get('/explore', listingController.showExplore);
+router.post('/explore', catchErrors(listingController.searchListings));
+
 router.get('/logout', authController.logout);
 
+router.get('/seed', listingController.seedDB);
 
 module.exports = router;
